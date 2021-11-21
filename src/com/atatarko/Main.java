352d48b8e2309c36e99ceeeb80fc7b3268147901
Anastasiya Tatarko <fhgjhdgj@gmail.com>
2021-11-21 23:14:44 +0200

    Initial commit

--- src/com/atatarko/Main.java
+++ src/com/atatarko/Main.java
@@ -3,6 +3,6 @@ package com.atatarko;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("My first work with GIT");
+        System.out.println("My first work with GIT:");
     }
 }
